31f40e537605844634576786fee8446343f9d19a
Benjamin Geer <benjamin.geer@runbox.com>
2020-06-05 17:54:12 +0200

    Update site

--- deploy.sh
+++ deploy.sh
@@ -8,22 +8,20 @@ hugo
 cp -a public/en/. docs/
 cp -a public/fr/. ../socioresources-fr/docs/
 cp -a public/ar/. ../socioresources-ar/docs/
-
 rm -rf public
-
-exit 1
 
 git add -A
 git commit -m "Update site"
 git push origin master
+
 pushd ../socioresources-fr
 git add -A
 git commit -m "Update site"
 git push origin master
 popd
+
 pushd ../socioresources-ar
 git add -A
 git commit -m "Update site"
 git push origin master
 popd
-
